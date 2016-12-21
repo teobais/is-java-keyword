efd6e63fb00e46101e01952077f509c7cc1449fb
Thodoris Bais <thodoris.bais@gmail.com>
2016-12-21 01:26:12 +0100

    Initial commit

---
+++ spec/boilerplate-spec.js
@@ -0,0 +1,13 @@
+var boilerplate = require('../index');
+
+describe('#boilerplate', function() {
+    
+    describe("Testing a function", function () {
+        it('function should exist', function() {
+            expect(boilerplate.boil).toBeDefined();
+        });
+    });
+    
+});
+
+   
